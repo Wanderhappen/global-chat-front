bdd2db083d46ee3113b5f519c7d2aad330009ac9
Wanderhappen <miracle12301230@gmail.com>
2024-10-16 17:05:38 +0400

    change url server to render.com

--- src/features/messageList/api/api.ts
+++ src/features/messageList/api/api.ts
@@ -3,7 +3,7 @@
 import axios from 'axios'
 import { io, Socket } from 'socket.io-client'
 
-const socketUrl = 'http://localhost:3009'
+const socketUrl = 'https://chat-y607.onrender.com'
 
 export const chatApi = {
   socket: null as null | Socket,
